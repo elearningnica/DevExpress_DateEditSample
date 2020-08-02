24c31b16a325d4ccdc89838e0e247ac61d1bfff7
Milton Navarro <milton.j.navarro.p@gmail.com>
2020-08-02 14:57:16 -0600

    Add project files.

---
+++ DevExpress_DateEditSample/Controllers/DateEditController.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevExpress_DateEditSample.Controllers
+{
+    public class DateEditController : Controller
+    {
+        // GET: DateEdit
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index ([ModelBinder(typeof(DevExpressEditorsBinder))] DateTime dvDE)
+        {
+            var date = dvDE.Date;
+            var time = dvDE.TimeOfDay;
+
+            return View();
+        }
+    }
+}
